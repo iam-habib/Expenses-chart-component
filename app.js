83e89e93ed61fab82bb5aaebc346c4cbd4f23712
Habib <92280144+iam-habib@users.noreply.github.com>
2022-11-03 23:48:09 +0530

    Add files via upload
    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+import data from "./data.json" assert { type: "json" };
+
+const bars = document.querySelectorAll(".bar");
+const spend = document.querySelectorAll(".spend");
+const days = document.querySelectorAll(".day");
+
+for (let i = 0; days.length > i; i++) {
+  const height = data[i].amount / 7;
+
+  console.log(height);
+  days[i].textContent = data[i].day;
+  spend[i].textContent = `$${data[i].amount}`;
+  bars[i].style.height = `${height}em`;
+
+  bars[i].addEventListener("click", function () {
+    spend[i].classList.toggle("active");
+    bars[i].classList.toggle("active_bar");
+  });
+}
